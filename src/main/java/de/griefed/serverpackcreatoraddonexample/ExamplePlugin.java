7047b1753f089f91c604b42d8a0dd568476e3139
Griefed <griefed@griefed.de>
2022-01-31 17:48:49 +0100

    build: Alternative way of writing extensions. Provide example which gathers plugin information from manifest and logs accordingly.

--- src/main/java/de/griefed/serverpackcreatoraddonexample/ExamplePlugin.java
+++ src/main/java/de/griefed/serverpackcreatoraddonexample/ExamplePlugin.java
@@ -1,259 +1,85 @@
 package de.griefed.serverpackcreatoraddonexample;
 
-import de.griefed.serverpackcreator.ApplicationProperties;
-import de.griefed.serverpackcreator.ConfigurationModel;
-import de.griefed.serverpackcreator.plugins.serverpackhandler.PostGenExtension;
-import de.griefed.serverpackcreator.plugins.serverpackhandler.PreZipExtension;
-import de.griefed.serverpackcreator.plugins.serverpackhandler.PreGenExtension;
-import de.griefed.serverpackcreator.plugins.swinggui.TabExtension;
-import de.griefed.serverpackcreator.utilities.ConfigUtilities;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringUtils;
+import de.griefed.serverpackcreator.DefaultFiles;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.pf4j.Extension;
 import org.pf4j.Plugin;
 import org.pf4j.PluginWrapper;
+import org.springframework.boot.system.ApplicationHome;
 
-import javax.swing.*;
-import java.awt.*;
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 
 public class ExamplePlugin extends Plugin {
 
     private static final Logger LOG_ADDONS = LogManager.getLogger("AddonsLogger");
+    private static final Logger LOG = LogManager.getLogger(ExamplePlugin.class);
 
-    /*
-        You should provide and read all these from the manifest.
-                                                                    */
-    public static final String NAME = "ServerPackCreatorExampleAddon";
-    public static final String DESCRIPTION = "This is an example addon for ServerPackCreator showcasing all, currently 4, aspects of" +
-            "ServerPackCreators addon functionality. In this example you see code which gets executed before a server pack is generated," +
-            "code that gets executed after a server pack was generated but before the ZIP-archive is created, code that gets executed" +
-            "after this ZIP-archive was created, as well as code adding a new tabbed pane to the GUI.";
-    public static final String AUTHOR = "Griefed";
-    public static final String VERSION = "0.0.1";
+    public static String NAME;
+    public static String PROVIDER;
+    public static String AUTHOR;
+    public static String VERSION;
 
     public ExamplePlugin(PluginWrapper wrapper) {
         super(wrapper);
+
+        JarFile jarFile = null;
+        Manifest manifest = null;
+
+        try {
+            jarFile = new JarFile(new File(new ApplicationHome(this.getClass()).getSource().toString().replace("\\","/")));
+        } catch (IOException ex) {
+            LOG_ADDONS.error("Couldn't acquire JarFile.",ex);
+        }
+
+        if (jarFile != null) {
+            for (Enumeration<JarEntry> entries = jarFile.entries(); entries.hasMoreElements();) {
+
+                JarEntry jarEntry = entries.nextElement();
+                String entryName = jarEntry.getName();
+
+                if (entryName.equals("META-INF/MANIFEST.MF")) {
+
+                    try (InputStream inputStream = jarFile.getInputStream(jarEntry)) {
+                        manifest = new Manifest(inputStream);
+                        break;
+                    } catch (Exception ex) {
+                        LOG_ADDONS.error("Couldn't read inputstream for Manifest.",ex);
+                    }
+                }
+            }
+        }
+
+        if (manifest != null) {
+
+            NAME = manifest.getMainAttributes().getValue("Plugin-Name");
+            PROVIDER = manifest.getMainAttributes().getValue("Plugin-Description");
+            AUTHOR = manifest.getMainAttributes().getValue("Plugin-Provider");
+            VERSION = manifest.getMainAttributes().getValue("Plugin-Version");
+
+        }
     }
 
     @Override
     public void start() {
+
+        //Log to serverpackcreator.log
+        LOG.info("Plugin-Name:        " + NAME);
+        LOG.info("Plugin-Description: " + PROVIDER);
+        LOG.info("Plugin-Provider:    " + AUTHOR);
+        LOG.info("Plugin-Version:     " + VERSION);
+
+        //Log to addons.log
         LOG_ADDONS.info("Starting ExamplePlugin...");
         LOG_ADDONS.info("This methods should prepare the environment for anything you want to do with it.");
         LOG_ADDONS.info("You could download some files. Create or replace some files. Basically you can do whatever you want.");
         /*
             Write all your preparation code here..
                                                     */
-    }
-
-    @Extension(ordinal = 1)
-    public static class ExampleStartExtension implements PreGenExtension {
-
-        @Override
-        public void run(ApplicationProperties applicationProperties, ConfigurationModel configurationModel, String destination) throws Exception {
-            LOG_ADDONS.info("This would run before a server pack generation.");
-            LOG_ADDONS.info("Received destination: " + destination);
-            LOG_ADDONS.info("We recieved the following configurationModel: " + configurationModel.toString());
-            LOG_ADDONS.info("We received the following applicationProperties: " + applicationProperties.toString());
-            StringUtils.upperCase("some text in lower case");
-            //Create example file in server pack
-            try {
-            Files.createFile(Paths.get(String.format("%s/%s",destination, this.getClass().getSimpleName())));
-            } catch (Exception ignored) {}
-            /*
-                Write all your pre-gen stuff here...
-                                                        */
-        }
-
-        @Override
-        public String getName() {
-            return NAME;
-        }
-
-        @Override
-        public String getDescription() {
-            return DESCRIPTION;
-        }
-
-        @Override
-        public String getAuthor() {
-            return AUTHOR;
-        }
-
-        @Override
-        public String getVersion() {
-            return VERSION;
-        }
-    }
-
-    @Extension(ordinal = 1)
-    public static class ExampleCreatedExtension implements PreZipExtension {
-
-        @Override
-        public void run(ApplicationProperties applicationProperties, ConfigurationModel configurationModel, String destination) throws Exception {
-            LOG_ADDONS.info("This would run after a server pack was generated, but BEFORE the ZIP-archive would be generated.");
-            LOG_ADDONS.info("Received destination: " + destination);
-            LOG_ADDONS.info("We recieved the following configurationModel: " + configurationModel.toString());
-            LOG_ADDONS.info("We received the following applicationProperties: " + applicationProperties.toString());
-            StringUtils.upperCase("some text in lower case");
-            //Create example file in server pack
-            try {
-                FileUtils.createParentDirectories(new File(destination + "/some/folder/with/a/name"));
-            } catch (Exception ex) {
-                LOG_ADDONS.info("Error occurred creating parent directories.", ex);
-            }
-
-            new ConfigUtilities(null,null,null,applicationProperties, null)
-                    .writeConfigToFile(configurationModel, new File(destination + "/serverpackcreator.conf"));
-            /*
-                Write all your post-gen-pre-zip stuff here...
-                                                                 */
-        }
-
-        @Override
-        public String getName() {
-            return NAME;
-        }
-
-        @Override
-        public String getDescription() {
-            return DESCRIPTION;
-        }
-
-        @Override
-        public String getAuthor() {
-            return AUTHOR;
-        }
-
-        @Override
-        public String getVersion() {
-            return VERSION;
-        }
-    }
-
-    @Extension(ordinal = 1)
-    public static class ExampleArchiveExtension implements PostGenExtension {
-
-        @Override
-        public void run(ApplicationProperties applicationProperties, ConfigurationModel configurationModel, String destination) throws Exception {
-            LOG_ADDONS.info("This would run after the server pack ZIP-archive was created.");
-            LOG_ADDONS.info("Received destination: " + destination);
-            LOG_ADDONS.info("We recieved the following configurationModel: " + configurationModel.toString());
-            LOG_ADDONS.info("We received the following applicationProperties: " + applicationProperties.toString());
-            //Create example file in server pack
-            try {
-                Files.createFile(Paths.get(String.format("%s/%s",destination, this.getClass().getSimpleName())));
-            } catch (Exception ignored) {}
-
-            /*
-                Write all your post-archive stuff here...
-                                                        */
-        }
-
-        @Override
-        public String getName() {
-            return NAME;
-        }
-
-        @Override
-        public String getDescription() {
-            return DESCRIPTION;
-        }
-
-        @Override
-        public String getAuthor() {
-            return AUTHOR;
-        }
-
-        @Override
-        public String getVersion() {
-            return VERSION;
-        }
-    }
-
-    @Extension(ordinal = 1)
-    public static class ExampleAddTabExtension extends JComponent implements TabExtension {
-
-        @Override
-        public JComponent getTab() {
-            JComponent jComponent = new JPanel(false);
-            jComponent.setLayout(new GridBagLayout());
-
-            GridBagConstraints gridBagConstraints = new GridBagConstraints();
-            gridBagConstraints.fill = GridBagConstraints.NONE;
-            gridBagConstraints.anchor = GridBagConstraints.CENTER;
-            gridBagConstraints.gridwidth = 1;
-            gridBagConstraints.gridheight = 1;
-            gridBagConstraints.weightx = 1;
-            gridBagConstraints.weighty = 1;
-
-            JLabel jLabel = new JLabel("This is a label");
-            jLabel.setToolTipText("Some label tooltip");
-            gridBagConstraints.gridx = 1;
-            gridBagConstraints.gridy = 1;
-            jComponent.add(jLabel, gridBagConstraints);
-
-            JButton jButton = new JButton("Some button");
-            jButton.setToolTipText("This could do anything");
-            gridBagConstraints.gridx = 1;
-            gridBagConstraints.gridy = 2;
-            jComponent.add(jButton, gridBagConstraints);
-
-            return jComponent;
-        }
-
-        @Override
-        public Icon getTabIcon() {
-            /*
-             * other default icons can be:
-             *   OptionPane.errorIcon
-             *   OptionPane.warningIcon
-             *   OptionPane.questionIcon
-            */
-            return UIManager.getIcon("OptionPane.informationIcon");
-        }
-
-        @Override
-        public String getTabTitle() {
-            return "Some title text";
-        }
-
-        @Override
-        public String getTabTooltip() {
-            return "Some tooltip text";
-        }
-
-        @Override
-        public void run(ApplicationProperties applicationProperties, ConfigurationModel configurationModel, String destination) throws Exception {
-            /*
-                So far, the run(...) method for AddTab extensions never gets called.
-                There is no point in writing code here with the intention of
-                ServerPackCreator executing it.
-                                                        */
-        }
-
-        @Override
-        public String getName() {
-            return NAME;
-        }
-
-        @Override
-        public String getDescription() {
-            return DESCRIPTION;
-        }
-
-        @Override
-        public String getAuthor() {
-            return AUTHOR;
-        }
-
-        @Override
-        public String getVersion() {
-            return VERSION;
-        }
     }
 }
